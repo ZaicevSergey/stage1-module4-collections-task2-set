c6a45a72fdfd5246246b03460697122bcb64044d
ZaycevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2023-04-20 17:45:48 +0300

    solved

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,14 +5,15 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        Set<String> intersection = new HashSet<>(firstSet);
-        intersection.retainAll(secondSet);
+        Set<String> result = new HashSet<>(firstSet);
+        result.retainAll(secondSet);
+        result.removeAll(thirdSet);
 
-        Set<String> uniqueThirdSetElements = new HashSet<>(thirdSet);
-        uniqueThirdSetElements.removeAll(intersection);
+        Set<String> thirdSetOnly = new HashSet<>(thirdSet);
+        thirdSetOnly.removeAll(firstSet);
+        thirdSetOnly.removeAll(secondSet);
 
-        Set<String> result = new HashSet<>(intersection);
-        result.removeAll(uniqueThirdSetElements);
+        result.addAll(thirdSetOnly);
 
         return result;
     }
